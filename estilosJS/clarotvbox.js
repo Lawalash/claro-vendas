cfe5723055c7015d72b48e09731e0a7feccf9359
Lawalash <104632797+Lawalash@users.noreply.github.com>
2024-02-08 23:28:32 -0300

    att

--- estilosJS/clarotvbox.js
+++ estilosJS/clarotvbox.js
@@ -6,3 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
         claroTvBoxTable.style.display = (claroTvBoxTable.style.display === 'none' || claroTvBoxTable.style.display === '') ? 'block' : 'none';
     });
 });
+
+function abrirImagem() {
+    var modal = document.getElementById('imagemModal');
+    var imagem = document.getElementById('imagemModalSrc');
+    imagem.src = 'imagens/combo.jpg';
+    modal.style.display = 'block';
+}
+
+function fecharImagem() {
+    var modal = document.getElementById('imagemModal');
+    modal.style.display = 'none';
+}
