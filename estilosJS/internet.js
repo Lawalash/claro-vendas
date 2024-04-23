cf1c5d16dca07fec3ade30a14d9359f42b4dbff6
Lawalash <104632797+Lawalash@users.noreply.github.com>
2024-04-22 23:33:38 -0300

    atualizado

--- estilosJS/internet.js
+++ estilosJS/internet.js
@@ -18,3 +18,67 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// PLANO 1 GIGA
+// Função para abrir o modal ao passar o mouse e clicar na tabela
+document.getElementById('plan1').addEventListener('click', function(event) {
+    if (event.target.tagName.toLowerCase() !== 'th') { // Verifica se o elemento clicado não é o th
+        document.getElementById('janelaModal1').style.display = 'block'; // Usando 'janelaModal1' para o modal do plano 1 GIGA
+    }
+});
+
+// Função para fechar o modal ao clicar no botão de fechar dentro do modal
+document.querySelector('#janelaModal1 .close').addEventListener('click', function() {
+    document.getElementById('janelaModal1').style.display = 'none'; // Usando 'janelaModal1' para o modal do plano 1 GIGA
+});
+
+
+
+
+// PLANO 750 MEGAS
+// Função para abrir o modal do plano de 750 Mega ao passar o mouse e clicar na tabela
+document.getElementById('plan750Mega').addEventListener('click', function() {
+    document.getElementById('janelaModal750Mega').style.display = 'block'; // Usando 'janelaModal750Mega' para o modal do plano 750 Mega
+});
+
+// Função para fechar o modal ao clicar no botão de fechar
+document.getElementsByClassName('close')[1].addEventListener('click', function() {
+    document.getElementById('janelaModal750Mega').style.display = 'none'; // Usando 'janelaModal750Mega' para o modal do plano 750 Mega
+});
+
+
+// PLANO 500 MEGAS
+// Função para abrir o modal do plano de 500 Mega ao passar o mouse e clicar na tabela
+document.getElementById('plan500Mega').addEventListener('click', function() {
+    document.getElementById('janelaModal500Mega').style.display = 'block'; // Usando 'janelaModal500Mega' para o modal do plano 500 Mega
+});
+
+// Função para fechar o modal ao clicar no botão de fechar dentro do modal do plano de 500 Mega
+document.getElementById('janelaModal500Mega').getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('janelaModal500Mega').style.display = 'none'; // Usando 'janelaModal500Mega' para o modal do plano 500 Mega
+});
+
+
+// PLANO 350 MEGAS
+// Função para abrir o modal do plano de 350 Mega ao passar o mouse e clicar na tabela
+document.getElementById('plan350Mega').addEventListener('click', function() {
+    document.getElementById('janelaModal350Mega').style.display = 'block'; // Usando 'janelaModal350Mega' para o modal do plano 350 Mega
+});
+
+// Função para fechar o modal ao clicar no botão de fechar dentro do modal do plano de 350 Mega
+document.getElementById('janelaModal350Mega').getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('janelaModal350Mega').style.display = 'none'; // Usando 'janelaModal350Mega' para o modal do plano 350 Mega
+});
+
+
+// PLANO 250 MEGAS
+// Função para abrir o modal do plano de 250 Mega ao passar o mouse e clicar na tabela
+document.getElementById('plan250Mega').addEventListener('click', function() {
+    document.getElementById('janelaModal250Mega').style.display = 'block'; // Usando 'janelaModal250Mega' para o modal do plano 250 Mega
+});
+
+// Função para fechar o modal ao clicar no botão de fechar dentro do modal do plano de 250 Mega
+document.getElementById('janelaModal250Mega').getElementsByClassName('close')[0].addEventListener('click', function() {
+    document.getElementById('janelaModal250Mega').style.display = 'none'; // Usando 'janelaModal250Mega' para o modal do plano 250 Mega
+});
